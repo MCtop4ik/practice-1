ddf262105a3b12432db09b64548b62bc529cb74e
MCtop4ik <senya.zakharov.0808@mail.ru>
2025-09-20 20:53:43 +0300

    feat: solved problem

--- main.cpp
+++ main.cpp
@@ -1,6 +1,102 @@
+#include <cstring>
+#include <fstream>
 #include <iostream>
+#include <ostream>
+#include <stdexcept>
 
-int main(int argc, char** argv) {
-  std::cout << "Hello world\n";
-  return 0;
+#define unless(cond) if (!(cond))
+
+char *parseArg(int argc, char *argv[], const char *targetArg) {
+  bool isTarget = false;
+  char *argument;
+  for (int i = 0; i < argc; i++) {
+    if (isTarget) {
+      argument = argv[i];
+      break;
+    }
+    if (!std::strcmp(argv[i], targetArg)) {
+      isTarget = true;
+    }
+  }
+  return argument;
+}
+
+class String {
+private:
+  char str[64];
+  size_t ln;
+
+public:
+  String(char *initStr) { 
+    this->ln = strlen(initStr); 
+    strncpy(str, initStr, ln);
+    str[ln] = '\0';
+  }
+
+  char operator[](int index) {
+    if (index >= this->ln || index < 0)
+      throw std::out_of_range("Wrong index for String");
+    return str[index];
+  }
+
+  void log() const {
+    std::cout << "String: " << this->str << std::endl;
+    std::cout << "String Length: " << this->ln << std::endl;
+  }
+
+  char *getChars() { return str; }
+
+  size_t getLength() const { return this->ln; }
+
+  bool includes(String other) {
+    int mask1[26];
+    int mask2[26];
+          
+    for (int i = 0; i < 26; i++) mask1[i] = 0;
+    for (int i = 0; i < 26; i++) mask2[i] = 0;
+
+    for (int i = 0; i < this->getLength(); i++) {
+      mask1[str[i] - 'a'] = 1;
+    }
+
+    for (int i = 0; i < other.getLength(); i++) {
+      mask2[other[i] - 'a'] = 1;
+    }
+
+    for (int i = 0; i < 26; i++) {
+      unless (mask1[i] <= mask2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+int main(int argc, char *argv[]) {
+  String word = String(parseArg(argc, argv, "--word"));
+  String filepath = String(parseArg(argc, argv, "--file"));
+
+  char parsedStr[64];
+  int ln = 0;
+  
+  char ch;
+  int cnt = 0;
+  std::ifstream in;
+  in.open(filepath.getChars());
+  while (in.get(ch)) {
+    if (ch >= 'a' && ch <= 'z') {
+      parsedStr[ln] = ch;
+      ++ln;
+    } else {
+      parsedStr[ln] = '\0';
+      if (word.includes(String(parsedStr))) {
+        ++cnt;
+      };
+      ln = 0;
+    }
+  }
+
+  std::cout << cnt << std::endl;
+
+  return EXIT_SUCCESS;
 }

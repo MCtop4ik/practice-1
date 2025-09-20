06def9d35dddac0e3531cbfa616eb7c3d75da825
MCtop4ik <senya.zakharov.0808@mail.ru>
2025-09-20 21:09:10 +0300

    fix: change prefix increment except postfix

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 char *parseArg(int argc, char *argv[], const char *targetArg) {
   bool isTarget = false;
   char *argument = "";
-  for (int i = 0; i < argc; i++) {
+  for (int i = 0; i < argc; ++i) {
     if (isTarget) {
       argument = argv[i];
       break;

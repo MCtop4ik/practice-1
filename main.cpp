84dab099cd1ecf99e31779f11307e881de342233
MCtop4ik <senya.zakharov.0808@mail.ru>
2025-09-21 17:40:28 +0300

    fix: change camelCase functions in PascalCase according to google codestyle

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 #define unless(cond) if (!(cond))
 
-char *parseArg(int argc, char *argv[], const char *targetArg) {
+char *ParseArg(int argc, char *argv[], const char *targetArg) {
   bool isTarget = false;
   char *argument = {};
   for (int i = 0; i < argc; ++i) {
@@ -21,16 +21,16 @@ char *parseArg(int argc, char *argv[], const char *targetArg) {
 }
 
 namespace StringUtils {
-  bool isLowerCaseLetter(char ch) {
+  bool IsLowerCaseLetter(char ch) {
     return (ch >= 'a' && ch <= 'z');
   }
 
-  bool isUpperCaseLetter(char ch) {
+  bool IsUpperCaseLetter(char ch) {
     return (ch >= 'A' && ch <= 'Z');
   }
 
-  bool isLetter(char ch) {
-    return isLowerCaseLetter(ch) || isUpperCaseLetter(ch);
+  bool IsLetter(char ch) {
+    return IsLowerCaseLetter(ch) || IsUpperCaseLetter(ch);
   }
 }
 
@@ -51,50 +51,50 @@ public:
     return str[index];
   }
 
-  char at(int index) const {
+  char At(int index) const {
     if (index >= ln || index < 0)
       throw std::out_of_range("Wrong index for String");
     return str[index];
   }
 
-  void log() const {
+  void Log() const {
     std::cout << "String: " << str << std::endl;
     std::cout << "String Length: " << ln << std::endl;
   }
 
-  char *getChars() { return str; }
+  char *GetChars() { return str; }
 
-  size_t getLength() const { return ln; }
+  size_t GetLength() const { return ln; }
 
-  void initMask(int* mask) {
+  void InitMask(int* mask) {
     for (int i = 0; i < kEnglishAlphabetLength; ++i) {
       mask[i] = 0;
     }
   }
 
-  void createMask(const String& maskStr, int* mask) {
+  void CreateMask(const String& maskStr, int* mask) {
     char sym = {};
     const unsigned int kUpperLettersShift = kEnglishAlphabetLength;
-    for (int i = 0; i < static_cast<int>(maskStr.getLength()); i++) {
-      sym = maskStr.at(i);
-      if (StringUtils::isLowerCaseLetter(sym)) {
+    for (int i = 0; i < static_cast<int>(maskStr.GetLength()); i++) {
+      sym = maskStr.At(i);
+      if (StringUtils::IsLowerCaseLetter(sym)) {
         mask[sym - 'a'] = 1;
       }
-      if (StringUtils::isUpperCaseLetter(sym)) {
+      if (StringUtils::IsUpperCaseLetter(sym)) {
         mask[sym - 'A' + kUpperLettersShift] = 1;
       }
     }
   }
 
-  bool includes(const String& other) {
+  bool Includes(const String& other) {
     int mask1[54];
     int mask2[54];
 
-    initMask(mask1);
-    initMask(mask2);
+    InitMask(mask1);
+    InitMask(mask2);
           
-    createMask(*this, mask1);
-    createMask(other, mask2);
+    CreateMask(*this, mask1);
+    CreateMask(other, mask2);
 
     for (int i = 0; i < kEnglishAlphabetLength; ++i) {
       unless (mask1[i] <= mask2[i]) {
@@ -108,8 +108,8 @@ public:
 
 
 int main(int argc, char *argv[]) {
-  String kWord = String(parseArg(argc, argv, "--word"));
-  String kFilepath = String(parseArg(argc, argv, "--file"));
+  String kWord = String(ParseArg(argc, argv, "--word"));
+  String kFilepath = String(ParseArg(argc, argv, "--file"));
 
   char parsedStr[64];
   unsigned int lengthStr = 0;
@@ -118,15 +118,15 @@ int main(int argc, char *argv[]) {
   
   char inputChar;
   std::ifstream in;
-  in.open(kFilepath.getChars());
+  in.open(kFilepath.GetChars());
   while (in.get(inputChar)) {
-    if (StringUtils::isLetter(inputChar)) {
+    if (StringUtils::IsLetter(inputChar)) {
       parsedStr[lengthStr] = inputChar;
       ++lengthStr;
     } else {
       parsedStr[lengthStr] = '\0';
       lengthStr = 0;
-      if (kWord.includes(String(parsedStr))) {
+      if (kWord.Includes(String(parsedStr))) {
         ++cnt;
       };
     }
